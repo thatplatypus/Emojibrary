4ed71184a95e1e9d69be5bf8f13aaf75f9027fce
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-11-01 17:16:09 -0600

    Update home breadcrumb to use emoji

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -20,25 +20,12 @@
 {#if !isHome}
   <div class="breadcrumbs mb-4">
     <ul>
-      <li>
-        <a href="{base}/" class="flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-5"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-            />
-          </svg>
-          <span>Home</span>
-        </a>
-      </li>
+			<li>
+				<a href="{base}/" class="flex items-center gap-2">
+					<span class="text-lg">🏠</span>
+					<span>Home</span>
+				</a>
+			</li>
       <li>
         <div class="flex items-center gap-2">
           {#if emoji}
